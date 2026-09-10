64a606e4e71f83f9e80309e51733c51ae25f469b
Farnam <farnamrajabi028@gmail.com>
2026-09-11 00:46:51 +0330

    feat(admin): configure todo management panel

--- todoapplication/admin.py
+++ todoapplication/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "user",
+        "is_completed",
+        "start_date",
+        "end_date",
+        "deadline",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_completed",
+        "created_at",
+        "start_date",
+        "end_date",
+        "deadline",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+        "user__username",
+        "user__email",
+    )
+
+    ordering = ("-created_at",)
+
+    list_select_related = ("user",)
+
+    readonly_fields = ("created_at",)
+
+    autocomplete_fields = ("user",)
+
+    list_per_page = 25
